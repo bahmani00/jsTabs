79223a31c63db649123fb8473df10b23679be2c9
Daniel Bahmani <bahmani00@gmail.com>
2015-06-24 20:00:15 -0400

    Immediately-Invoked Function Expression (IIFE)

--- tab.v1.js
+++ tab.v1.js
@@ -1,1 +1,6 @@
-var myJsLib = myJsLib || {};
+//use Immediately-Invoked Function Expression (IIFE)
+var myJsLib = (function (myJsLib) {
+  
+  return myJsLib;
+}(myJsLib || {})); // if myJsLib doesn't exist pass an Object Literal
+
