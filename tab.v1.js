588f49ae2616e29f5ea57bf745fe7c950f301990
Daniel Bahmani <bahmani00@gmail.com>
2015-06-25 00:48:07 -0400

    add createTabStrip function

--- tab.v1.js
+++ tab.v1.js
@@ -1,6 +1,24 @@
 //use Immediately-Invoked Function Expression (IIFE)
 var myJsLib = (function (myJsLib) {
-  
-  return myJsLib;
-}(myJsLib || {})); // if myJsLib doesn't exist pass an Object Literal
 
+    myJsLib.createTabStrip = function (elementId) {
+        var elm = document.getElementById(elementId);
+        //if element dosnt exist create it
+        if (!elm) {
+            elm = document.createElement("DIV");
+            elm.id = elementId;
+        }
+
+        var tabs = elm.querySelectorAll(".tab");
+        var createTabs = function (tabs) {
+
+        };
+
+        return {
+            tabs: createTabs(tabs)
+        };
+
+    }
+
+    return myJsLib;
+}(myJsLib || {})); // if myJsLib doesn't exist pass an Object Literal
