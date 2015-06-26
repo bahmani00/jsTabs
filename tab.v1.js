e63dfafe46b74fc376fa333868956f5ae6353324
Daniel Bahmani <bahmani00@gmail.com>
2015-06-25 23:59:29 -0400

    add tab items

--- tab.v1.js
+++ tab.v1.js
@@ -1,10 +1,54 @@
 //use Immediately-Invoked Function Expression (IIFE)
 var myJsLib = (function (myJsLib) {
 
-    var createTabs = function (tabs) {
+    var createPageView = function (pageViewElement) {
 
+        return {
+            el: pageViewElement,
+
+        };
     };
-    
+
+    var createTabs = function (tabElements, pageViewElements) {
+        var tabs = [];
+
+        [].forEach.call(tabElements, function (el, index, array) {
+
+            var tab = {
+                el: el,
+                pageView: createPageView(pageViewElements[index]),
+
+                disable: function () {
+                    this.el.classList.add("disabled");
+                },
+                enable: function () {
+                    this.el.classList.remove("disabled");
+                },
+                isDisabled: function () {
+                    return this.el.classList.contains("disabled");
+                },
+                activate: function () {
+                    if (!this.isDisabled()) {
+                        this.el.classList.add("active");
+                    }
+                },
+                deactivate: function () {
+                    if (!this.isDisabled()) {
+                        this.el.classList.remove("active");
+                    }
+                },
+                isActive: function () {
+                    return this.el.classList.contains("active");
+                },
+
+            };
+
+            tabs.push(tab);
+        });
+
+        return tabs;
+    };
+
     myJsLib.createTabStrip = function (elementId) {
         var elm = document.getElementById(elementId);
         //if element dosnt exist create it
@@ -13,11 +57,15 @@ var myJsLib = (function (myJsLib) {
             elm.id = elementId;
         }
 
-        var tabs = elm.querySelectorAll(".tab");
+        var tabElements = elm.querySelectorAll(".tab");
+        var pageViewElements = elm.querySelectorAll(".pageview");
+
+        var tabs = createTabs(tabElements, pageViewElements);
 
 
         return {
-            tabs: createTabs(tabs)
+            tabs: tabs,
+            pageViews: tabs.pageViews
         };
 
     }
